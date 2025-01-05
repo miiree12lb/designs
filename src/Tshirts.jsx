ab2aa8ba983bc6a24a7e281c6c4080f9f0f833cd
Mireia <66952824+miiree12lb@users.noreply.github.com>
2025-01-06 00:49:46 +0100

    wander tshirt

--- src/Tshirts.jsx
+++ src/Tshirts.jsx
@@ -29,6 +29,10 @@ import kiss from "./assets/images/tshirts/kiss.png";
 import kiss2 from "./assets/images/tshirts/kiss2.png";
 import kiss3 from "./assets/images/tshirts/kiss3.png";
 
+import wander from "./assets/images/tshirts/wander.png";
+import wander2 from "./assets/images/tshirts/wander2.png";
+import wander3 from "./assets/images/tshirts/wander3.png";
+
 import ItemDisplayer from "./ItemDisplayer.jsx";
 import { useNavigate } from "react-router-dom";
 
@@ -41,6 +45,7 @@ export default function Tshirts() {
         { title: "Keep it simple", imgs: [kiss, kiss2, kiss3]},
         { title: "Science Fellows", imgs: [scienceImg, scienceImg2, scienceImg3, scienceImg4] },
         { title: "Adventure", imgs: [adventure, adventure2, adventure3] },
+        { title: "Not all who wander are lost", imgs: [wander, wander2, wander3]},
         { title: "Route 66", imgs: [route66] },
         { title: "BCN - Gaudí", imgs: [gaudi, gaudi2, gaudi3] },
         { title: "miiree12lb", imgs: [miiree12lbImg, miiree12lbImg2] },
